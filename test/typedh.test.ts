9f4a9c11b00ab17fb555371954d1a84aee4f967f
Ron Martinez <ramartin.net@gmail.com>
2021-04-26 13:45:18 -0400

    wip

--- test/typedh.test.ts
+++ test/typedh.test.ts
@@ -2,8 +2,65 @@ import type { TypedH } from "hyperapp"
 
 import { h as ha } from "hyperapp"
 
-type S = {}
-const hs: TypedH<S> = ha
+type S0 = boolean
+type S1 = true
+type S2 = false
+type S3 = 0
+type S4 = 2424
+type S5 = -123
+type S6 = number
+type S7 = ""
+type S8 = "hi"
+type S9 = string
+type S10 = {}
+type S11 = { [_: string]: any }
+type S12 = { [_: string]: any, foo: number }
+type S13 = { foo: number }
+type S14 = Set<any>
+type S15 = []
+type S16 = symbol
+type S17 = () => {}
+type S18 = null
+type S19 = undefined
 
-// $ExpectType VNode<S1>
-hs("br", {})
+const h0: TypedH<S0> = ha
+const h1: TypedH<S1> = ha
+const h2: TypedH<S2> = ha
+const h3: TypedH<S3> = ha
+const h4: TypedH<S4> = ha
+const h5: TypedH<S5> = ha
+const h6: TypedH<S6> = ha
+const h7: TypedH<S7> = ha
+const h8: TypedH<S8> = ha
+const h9: TypedH<S9> = ha
+const h10: TypedH<S10> = ha    // $ExpectError
+const h11: TypedH<S11> = ha    // $ExpectError
+const h12: TypedH<S12> = ha
+const h13: TypedH<S13> = ha
+const h14: TypedH<S14> = ha
+const h15: TypedH<S15> = ha
+const h16: TypedH<S16> = ha
+const h17: TypedH<S17> = ha
+const h18: TypedH<S18> = ha
+const h19: TypedH<S19> = ha
+
+h0("br", {})     // $ExpectType VNode<boolean>
+h1("br", {})     // $ExpectType VNode<true>
+h2("br", {})     // $ExpectType VNode<false>
+h3("br", {})     // $ExpectType VNode<0>
+h4("br", {})     // $ExpectType VNode<2424>
+h5("br", {})     // $ExpectType VNode<-123>
+h6("br", {})     // $ExpectType VNode<number>
+h7("br", {})     // $ExpectType VNode<"">
+h8("br", {})     // $ExpectType VNode<"hi">
+h9("br", {})     // $ExpectType VNode<string>
+h10("br", {})    // $ExpectType VNode<S10>
+h11("br", {})    // $ExpectType VNode<S11>
+h12("br", {})    // $ExpectType VNode<S12>
+h13("br", {})    // $ExpectType VNode<S13>
+h14("br", {})    // $ExpectType VNode<S14>
+h15("br", {})    // $ExpectType VNode<[]>
+h16("br", {})    // $ExpectType VNode<symbol>
+h17("br", {})    // $ExpectType VNode<S17>
+h18("br", {})    // $ExpectType VNode<null>
+h19("br", {})    // $ExpectType VNode<undefined>
