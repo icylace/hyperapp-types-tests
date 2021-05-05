02d16f1cdcf2d71b96f6798eaaca70c304c20cc6
Ron Martinez <ramartin.net@gmail.com>
2021-05-04 23:02:03 -0400

    wip

--- test/api/text.test.ts
+++ test/api/text.test.ts
@@ -20,29 +20,3 @@ text(Symbol())            // $ExpectError
 text(() => { })           // $ExpectError
 text(null)                // $ExpectType TextVNode
 text(undefined)           // $ExpectType TextVNode
-
-// text<number>()                    // $ExpectError
-// text<number>(true)                // $ExpectType TextVNode
-// text<number>(false)               // $ExpectType TextVNode
-// text<number>(0)                   // $ExpectType TextVNode
-// text<number>(2424)                // $ExpectType TextVNode
-// text<number>(-123)                // $ExpectType TextVNode
-// text<number>(-Infinity)           // $ExpectType TextVNode
-// text<number>(Infinity)            // $ExpectType TextVNode
-// text<number>(NaN)                 // $ExpectType TextVNode
-// text<number>("")                  // $ExpectType TextVNode
-// text<number>("hi")                // $ExpectType TextVNode
-// text<number>(new String(""))      // $ExpectType TextVNode
-// text<number>(new String("hi"))    // $ExpectType TextVNode
-// text<number>({})                  // $ExpectType TextVNode
-// text<number>(new Set())           // $ExpectType TextVNode
-// text<number>([])                  // $ExpectType TextVNode
-
-// // TODO:
-// text<number>(Symbol())            // $ExpectError
-
-// // TODO:
-// text<number>(() => { })           // $ExpectError
-
-// text<number>(null)                // $ExpectType TextVNode
-// text<number>(undefined)           // $ExpectType TextVNode
