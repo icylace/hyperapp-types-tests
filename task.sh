ff2cb8f844c8bda0b55d025331755ddd2dbd0a38
Ron Martinez <ramartin.net@gmail.com>
2022-04-23 17:44:20 -0400

    wip

--- task.sh
+++ task.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
+update_json() {
+  local filter="$1"
+  local file="$2"
+  local tmp="$(mktemp)"
+
+  if [ ! -f "$file" ] ; then
+    echo '{}' > "$file"
+  fi
+
+  jq "$filter" "$file" > "$tmp" && mv -f "$tmp" "$file"
+}
+
+# ------------------------------------------------------------------------------
+
 task:index() {
   local tasks=(
-    'clean'
     'reset'
     'test'
   )
@@ -16,39 +29,28 @@ task:index() {
 
 # ------------------------------------------------------------------------------
 
-task:clean() {
-  echo
-  echo "Cleaning the distribution folder..."
-  rm -fr ./dist && mkdir ./dist
-}
-
-# ------------------------------------------------------------------------------
-
-# From `webdev-scaffolding`.
-update_json() {
-  local filter="$1"
-  local file="$2"
-  local tmp="$(mktemp)"
-
-  if [ ! -f "$file" ] ; then
-    echo '{}' > "$file"
-  fi
-
-  jq "$filter" "$file" > "$tmp" && mv -f "$tmp" "$file"
-}
-
 task:reset() {
   echo
-  echo "Resetting dependencies..."
+  echo "Reinstalling dependencies..."
 
   rm ./package-lock.json
   rm -fr ./node_modules
 
   update_json '.dependencies = {} | .devDependencies = {}' ./package.json
 
-  npm install --save typescript @definitelytyped/dtslint hyperapp
+  local dev_modules=()
 
-  task:clean
+  # Language
+  dev_modules+=('typescript')
+  dev_modules+=('@definitelytyped/dtslint')
+
+  npm install --save-dev "${dev_modules[@]}"
+
+  local modules=()
+
+  modules+=('hyperapp')
+
+  npm install --save "${modules[@]}"
 }
 
 # ------------------------------------------------------------------------------
